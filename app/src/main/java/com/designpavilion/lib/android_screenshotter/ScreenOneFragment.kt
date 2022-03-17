d44d2e1ea0397f8499d3e138ae7d9456fc814fb6
Akintunde <akintunde_olanrewaju@yahoo.co.uk>
2022-03-17 16:40:38 +0200

    integrate to Firebase Remote Config

--- app/src/main/java/com/designpavilion/lib/android_screenshotter/ScreenOneFragment.kt
+++ app/src/main/java/com/designpavilion/lib/android_screenshotter/ScreenOneFragment.kt
@@ -1,16 +1,46 @@
 package com.designpavilion.lib.android_screenshotter
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+
 
 class ScreenOneFragment : Fragment(R.layout.fragment_screen_one) {
+
+    private val mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance().apply {
+        setDefaultsAsync(R.xml.remote_config_defaults)
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<Button>(R.id.nextButton).setOnClickListener {
-//            findNavController().navigate(ScreenOneFragmentDirections)
+            fetchAndSet(view)
+        }
+    }
+
+    private fun fetchAndSet(view: View) {
+        mFirebaseRemoteConfig.fetch(60).addOnCompleteListener(this.requireActivity() as Activity)
+        { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(
+                    requireContext(), "Fetch Succeeded",
+                    Toast.LENGTH_SHORT
+                ).show()
+                mFirebaseRemoteConfig.activate().addOnCompleteListener {
+                    view.findViewById<Button>(R.id.nextButton).text =
+                        mFirebaseRemoteConfig.getString("button_name")
+                }
+            } else {
+                Toast.makeText(
+                    requireContext(), "Fetch Failed",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
